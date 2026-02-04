dd3524eddf3ba0b24a85e33b14caf4a59678c881
Wichern94 <Mateusz.kabaja@gmail.com>
2026-02-04 20:09:29 +0100

    feat: ✨ Initialize project assets and data structure
    - Create cardData object with initial values
    - Import and configure all static assets

---
+++ src/data/cardData.js
@@ -0,0 +1,26 @@
+export const pairPresets = [
+	{
+		pairId: 1,
+		type: 'letter',
+		value: 'S',
+		word: 'Statek',
+	},
+	{
+		pairId: 1,
+		type: 'image',
+		value: 'Statek',
+		imagePath: '/src/assets/images/ship.png',
+	},
+	{
+		pairId: 2,
+		type: 'letter',
+		value: 'M',
+		word: 'Miecz',
+	},
+	{
+		pairId: 2,
+		type: 'image',
+		value: 'Miecz',
+		imagePath: '/src/assets/images/ship.png',
+	},
+];
