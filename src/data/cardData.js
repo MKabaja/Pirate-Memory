ac6bba1b8d90768ebbdcfdf9528b935b4cbf2659
Wichern94 <Mateusz.kabaja@gmail.com>
2026-02-06 13:07:52 +0100

    feat: initialize card pairs data and global CSS resets

--- src/data/cardData.js
+++ src/data/cardData.js
@@ -1,4 +1,5 @@
-export const pairPresets = [
+export const pairsPool = [
+	// PAR 1
 	{
 		pairId: 1,
 		type: 'letter',
@@ -9,8 +10,10 @@ export const pairPresets = [
 		pairId: 1,
 		type: 'image',
 		value: 'Statek',
-		imagePath: '/src/assets/images/ship.png',
+		imagePath: '/cards/ship.png',
 	},
+
+	// PAR 2
 	{
 		pairId: 2,
 		type: 'letter',
@@ -21,6 +24,188 @@ export const pairPresets = [
 		pairId: 2,
 		type: 'image',
 		value: 'Miecz',
-		imagePath: '/src/assets/images/ship.png',
+		imagePath: '/cards/sword.webp',
+	},
+
+	// PAR 3
+	{
+		pairId: 3,
+		type: 'letter',
+		value: 'C',
+		word: 'Czapka',
+	},
+	{
+		pairId: 3,
+		type: 'image',
+		value: 'Czapka',
+		imagePath: '/cards/hat.png',
+	},
+
+	// PAR 4
+	{
+		pairId: 4,
+		type: 'letter',
+		value: 'P',
+		word: 'Papuga',
+	},
+	{
+		pairId: 4,
+		type: 'image',
+		value: 'Papuga',
+		imagePath: '/cards/parrot.png',
+	},
+
+	// PAR 5
+	{
+		pairId: 5,
+		type: 'letter',
+		value: 'K',
+		word: 'Kraken ship',
+	},
+	{
+		pairId: 5,
+		type: 'image',
+		value: 'Kraken ship',
+		imagePath: '/cards/kraken-ship.png',
+	},
+
+	// PAR 6
+	{
+		pairId: 6,
+		type: 'letter',
+		value: 'W',
+		word: 'Wyspa',
+	},
+	{
+		pairId: 6,
+		type: 'image',
+		value: 'Wyspa',
+		imagePath: '/cards/island.png',
+	},
+
+	// PAR 7
+	{
+		pairId: 7,
+		type: 'letter',
+		value: 'F',
+		word: 'Flaga',
+	},
+	{
+		pairId: 7,
+		type: 'image',
+		value: 'Flaga',
+		imagePath: '/cards/flag.jpg',
+	},
+
+	// PAR 8
+	{
+		pairId: 8,
+		type: 'letter',
+		value: 'Z',
+		word: 'Złoto',
+	},
+	{
+		pairId: 8,
+		type: 'image',
+		value: 'Złoto',
+		imagePath: '/cards/gold.png',
+	},
+
+	// PAR 9
+	{
+		pairId: 9,
+		type: 'letter',
+		value: 'O',
+		word: 'Opaska',
+	},
+	{
+		pairId: 9,
+		type: 'image',
+		value: 'Opaska',
+		imagePath: '/cards/band.png',
+	},
+
+	// PAR 10
+	{
+		pairId: 10,
+		type: 'letter',
+		value: 'L',
+		word: 'Lina',
+	},
+	{
+		pairId: 10,
+		type: 'image',
+		value: 'Lina',
+		imagePath: '/cards/rope.png',
+	},
+
+	// PAR 11
+	{
+		pairId: 11,
+		type: 'letter',
+		value: 'B',
+		word: 'Beczka',
+	},
+	{
+		pairId: 11,
+		type: 'image',
+		value: 'Beczka',
+		imagePath: '/cards/barrel.webp',
+	},
+
+	// PAR 12
+	{
+		pairId: 12,
+		type: 'letter',
+		value: 'A',
+		word: 'Ali Hoop',
+	},
+	{
+		pairId: 12,
+		type: 'image',
+		value: 'Ali Hoop',
+		imagePath: '/cards/ali-hoop.png',
+	},
+
+	// PAR 13
+	{
+		pairId: 13,
+		type: 'letter',
+		value: 'H',
+		word: 'Hak',
+	},
+	{
+		pairId: 13,
+		type: 'image',
+		value: 'Hak',
+		imagePath: '/cards/hook.png',
+	},
+
+	// PAR 14
+	{
+		pairId: 14,
+		type: 'letter',
+		value: 'D',
+		word: 'Dzwon',
+	},
+	{
+		pairId: 14,
+		type: 'image',
+		value: 'Dzwon',
+		imagePath: '/cards/bell.png',
+	},
+
+	// PAR 15
+	{
+		pairId: 15,
+		type: 'letter',
+		value: 'Ż',
+		word: 'Żagiel',
+	},
+	{
+		pairId: 15,
+		type: 'image',
+		value: 'Żagiel',
+		imagePath: '/cards/sail.png',
 	},
 ];
